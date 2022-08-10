1076f8b15725f5f676b5453a0863c5856379d2c3
DESKTOP-AndresGalvis <andres.felipe99@hotmail.com>
2022-08-10 15:15:37 -0500

    Adding favicon, copy of database and modify header.html showing the icon.

--- template/header.php
+++ template/header.php
@@ -2,17 +2,19 @@
 <html lang="es">
 
 <head>
-    <title>CRUD PHP, MYSQL y Bootstrap5</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <link rel="shortcut icon" href="./img/lapiz.ico" type="image/x-icon">
+
     <!-- Bootstrap CSS v5.2.0-beta1 -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <title>CRUD: PHP, MySQL & BS5</title>
 </head>
 
 <body>
